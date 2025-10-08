23134478afd4df4d4cb68666201089cb596fa266
Davi Mendes <119417537+DMendes7@users.noreply.github.com>
2025-10-08 15:20:56 -0300

    LAB03: Update urls fo midea in render

--- Projetos/Lab03 - Sistema de Moeda Estudantil/moeda_estudantil/urls.py
+++ Projetos/Lab03 - Sistema de Moeda Estudantil/moeda_estudantil/urls.py
@@ -8,14 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("apps.accounts.urls")),
     path("catalogo/", include("apps.catalog.urls")),
-
-    # ✅ VOLTA PARA O APP QUE JÁ FUNCIONAVA
     path("transacoes/", include("apps.transactions.urls")),
-
     path("empresas/", include("apps.partners.urls")),
     path("admin/", admin.site.urls),
 ]
 
-# Serve arquivos de mídia em DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
