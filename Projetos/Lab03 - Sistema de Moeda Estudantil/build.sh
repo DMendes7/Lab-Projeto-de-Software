ca72795c984e425583c42a84e1cdb5532dee2549
Davi Mendes <119417537+DMendes7@users.noreply.github.com>
2025-10-08 14:38:03 +0000

    LAB03: Update build.sh

--- Projetos/Lab03 - Sistema de Moeda Estudantil/build.sh
+++ Projetos/Lab03 - Sistema de Moeda Estudantil/build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# use o Python do runtime do Render
+# usar o Python do runtime do Render
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip show gunicorn || true
 
-# coletar estáticos e aplicar migrações
+# coletar estáticos
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+python manage.py migrate partners 0003 --no-input
+
+python manage.py migrate partners 0004 --fake --no-input || true
+
+python manage.py migrate --no-input
